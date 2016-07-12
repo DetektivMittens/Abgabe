a4384927135b7ecdc53d6b5de8637831753fe099
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-07-12 11:35:52 +0200

    add security vulnerability as a comment

--- Taschenrechner.rb
+++ Taschenrechner.rb
@@ -10,7 +10,13 @@ end
 
 post "/input" do
 result=params["numbers"]
-#eval params("numbers") #.send(:eval)
+
+# TODO: Never, never do something like this without sanitizing the user input
+# If you comment this in, the user is able to enter `File.read('/etc/passwd')`
+# and if the server is running on a unix machine, the content of the password
+# file is visible to the user
+# TODO: Google for 'remote code execution'
+# result=eval(params["numbers"])
 
 redirect to("/")
 end
